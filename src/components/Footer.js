74107a8468e80349a352e96d14a3c65cf46dfec5
Priyam <priyam05soni@gmail.com>
2022-01-12 13:02:15 +0530

    components file

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Copyright &copy; 2022</p>
+            <a href="https://github.com/Priyammm05">About</a>
+        </footer>
+    )
+}
+
+export default Footer
